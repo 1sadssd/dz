2a407bc6879f171c760d939a54880ce662fff370
aud404_st <aud404_st@CR404-PC16>
2023-01-22 09:50:13 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+int main()
+
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	/*Зарплата менеджера — 100$ + 5% від продажів. Користувач вводить з клавіатури загальну суму угод
+менеджера за місяць. Порахувати підсумкову зарплату
+менеджера.*/
+
+	double zp = 100;
+	double ygody;
+	double summa;
+
+	cout << "Зарплата менеджера без угод = " << zp << "" << '$';
+	cout << "\n";
+	cout << "Введіть загальну сумум угод : "; 
+	cin >> ygody;
+	cout<<"\n";
+
+	summa = (ygody + ygody / 100 * 5);
+
+	cout << "Зарплата з угодами = " << summa << "" << '$';
+
+	return 0;
+
+}
+
+
+			
+
+
+
